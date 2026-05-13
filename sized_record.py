23406b3e483df5bb645b294542253c6014ecec1f
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-13 20:00:22 +0300

    Initial import
    SizedRecord class, main guard

---
+++ sized_record.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import BinaryIO
+import struct
+import field as FM
+
+
+class SizedRecord:
+    def __init__(self, fd: BinaryIO, numpoly: int):
+        self.fd = fd
+        self.numpoly = numpoly
+        self.view: memoryview = fd.read()
+
+    def iter_as(self, fmt: str | FM.FieldMeta):
+        for n in range(self.numpoly):
+            (size,) = struct.unpack("<i", self.fd.read(4))
+            if isinstance(fmt, str):
+                for j in range(size // struct.calcsize("<dd")):
+                    yield struct.unpack("<dd", self.fd.read(struct.calcsize("<dd")))
+            elif isinstance(fmt, FM.FieldMeta):
+                for j in range(size // fmt._view_size):
+                    yield fmt(self.fd.read(fmt._view_size))
+
+
+if __name__ == "__main__":
+    with open("polys.bin", "rb") as f:
+        ph = FM.PolyHeader(f.read(FM.PolyHeader._view_size))
+        print(FM.as_tuple(ph))
+        # print(ph.min.x)
+        sr = SizedRecord(f, ph.numpoly)
+        for pp in sr.iter_as("<dd"):
+            print(pp)
